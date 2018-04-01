67e7a1b87125ce2d0863054facb3abc3f3d986ca
CloudCorp <37921410+CloudCorp@users.noreply.github.com>
2018-04-01 15:56:58 +1000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message) {
-	message.channel.send('Pong.');
-	message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");    
+	message.channel.send('Pong.')(new Date().getTime() - message.createdTimestamp + " ms");    
 	},	
 };
